11779ae4ffd30e44f5f670a72d2f1d8348d6d91d
Pragya <pragn@flemingcollege.ca>
2023-05-24 17:31:55 -0400

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py
    -Added two print() statement to script2.py

--- script2.py
+++ script2.py
@@ -1,2 +1,4 @@
 #Print lines from python 
 print('complex is better than complicated')
+print ('if the implementation is easy to explain, its a bad idea.')
+print('There should be one -- and preferaably only one --obvious way to do it.')
